a64e1ead394e792f42736d496b5e6d164f2cfe58
sh-pers <sandra_huett@persistent.com>
2021-06-02 13:14:19 +0200

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -56,6 +56,7 @@ define([
         }
 
         var url;
+        var content;
         var hasInArguments = Boolean(
             payload['arguments'] &&
             payload['arguments'].execute &&
