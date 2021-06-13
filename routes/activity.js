9c95364f713e8dd08164a25c7cf4f687570ef858
sh-pers <sandra_huett@persistent.com>
2021-06-13 12:01:05 +0200

    v0

--- routes/activity.js
+++ routes/activity.js
@@ -64,6 +64,7 @@ exports.edit = function (req, res) {
 exports.save = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     console.log( req.body );
+    console.log( 'TEST SAVE' );
     logData(req);
     res.send(200, 'Save');
 };
